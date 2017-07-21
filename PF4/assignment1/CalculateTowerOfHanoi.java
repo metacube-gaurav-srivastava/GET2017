0dc2340495f85a3f073efe589730acabe7e00577
meta-anushtha-gupta <anushtha.gupta@metacube.com>
2017-07-21 06:45:18 +0530

    Update CalculateTowerOfHanoi.java

--- PF4/assignment1/CalculateTowerOfHanoi.java
+++ PF4/assignment1/CalculateTowerOfHanoi.java
@@ -3,8 +3,8 @@ package assignment1;
  * Name			:	CalculateTowerOfHanoi
  * Author		:	Anushtha Gupta
  * Date			:	2017-07-18
- * Reference	:	Assignment 4 - Exercise 1
- * Description	:	This is used to generate all the moves used to transfer given 
+ * Reference		:	Assignment 4 - Exercise 1
+ * Description		:	This is used to generate all the moves used to transfer given 
  * 					number of disks from source to destination pole 
  */
 import java.util.*;
@@ -13,39 +13,39 @@ public class CalculateTowerOfHanoi {
 	
 	
 	/**
-	 * @param source		:	Source Pole
+	 * @param source	:	Source Pole
 	 * @param destination	:	Destination Pole 
-	 * @param temp			:	Temporary Pole
-	 * @param numberofdisks	:	Total number of disks in the source pole
-	 * @return				:	lists of moves for transferring disks from source to destination
+	 * @param temp		:	Temporary Pole
+	 * @param numberOfDisks	:	Total number of disks in the source pole
+	 * @return		:	lists of moves for transferring disks from source to destination
 	 */
 	public List<String> towerOfHanoi(String source, String destination,
-			String temp, int numberofdisks) {
-		if (numberofdisks <= 0) {
+			String temp, int numberOfDisks) {
+		if (numberOfDisks <= 0) {
 			throw new IllegalArgumentException();
 		} else {
 			/*
 			 * stores all the moves used to transfer given number of disks to
 			 * destination pole
 			 */
-			List<String> listwords = new ArrayList<String>();
+			List<String> listWords = new ArrayList<String>();
 
 			// Base condition when one disk is present
-			if (numberofdisks == 1) {
-				listwords.add("Move Disk 1 From " + source + " to "
+			if (numberOfDisks == 1) {
+				listWords.add("Move Disk 1 From " + source + " to "
 						+ destination);
-				return listwords;
+				return listWords;
 			}
 
 			// condition when there are more than one disks.
 			else {
-				listwords.addAll(towerOfHanoi(source, temp, destination,
-						numberofdisks - 1));
-				listwords.add("Move Disk " + numberofdisks + " From " + source
+				listWords.addAll(towerOfHanoi(source, temp, destination,
+						numberOfDisks - 1));
+				listWords.add("Move Disk " + numberOfDisks + " From " + source
 						+ " to " + destination);
-				listwords.addAll(towerOfHanoi(temp, destination, source,
-						numberofdisks - 1));
-				return listwords;
+				listWords.addAll(towerOfHanoi(temp, destination, source,
+						numberOfDisks - 1));
+				return listWords;
 			}
 		}
 	}
